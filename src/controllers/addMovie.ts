30fb729389ca9d2cffeead3ac072db42c9788c68
Browsero <hubertmadej02@gmail.com>
2022-06-02 00:01:30 +0200

    Fixed Post Schema

--- src/controllers/addMovie.ts
+++ src/controllers/addMovie.ts
@@ -3,6 +3,13 @@ import MovieTemplate, { validateMovie } from "../types/movie";
 import { v4 as uuidv4 } from "uuid";
 import Movie from "../handlers/Post";
 
+/**
+ * It takes a request and a response, and then it takes the body of the request and creates a
+ * new movie object. Then it validates the movie object and saves it to the movie
+ * database.
+ * @param {Request} req - Request, res: Response
+ * @param {Response} res - Response
+ */
 const addMovie = (req: Request, res: Response) => {
   const { name, year, runtime, director, actors, plot, posterURL, genres } =
     req.body;
@@ -16,12 +23,13 @@ const addMovie = (req: Request, res: Response) => {
     { actors, plot, posterURL },
     genres
   );
-  const ValidatedMovies = validateMovie(newMovie, newMovie.genres);
-  const movie = new Movie({
-    id: generatedId,
-    name: ValidatedMovies.name,
-  });
-  movie.save();
-  res.send("Success");
+  const ValidatedMovie = validateMovie(newMovie, newMovie.genres);
+  const movie = new Movie(ValidatedMovie);
+  movie
+    .save()
+    .then((response) => {
+      res.send(`The object was successfully added to the database. Movie ID: ${generatedId}`);
+    })
+    .catch((err) => console.error(err));
 };
 export default addMovie;
--- src/handlers/Post.ts
+++ src/handlers/Post.ts
@@ -3,11 +3,25 @@ import { Schema, model } from "mongoose";
 interface IMovie {
   id: string;
   name: string;
+  year: number;
+  runtime: number;
+  director: string;
+  actors?: string;
+  plot?: string;
+  posterURL?: string;
+  genres: string[];
 }
 
 const movieSchema = new Schema<IMovie>({
   id: { type: String, required: true },
   name: { type: String, required: true },
+  year: { type: Number, required: true },
+  runtime: { type: Number, required: true },
+  director: { type: String, required: true },
+  actors: { type: String },
+  plot: { type: String },
+  posterURL: { type: String },
+  genres: [{ type: String, required: true }],
 });
 
 const Movie = model("Movie", movieSchema);
